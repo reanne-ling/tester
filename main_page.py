32d1d1d2021fdd7d1a832d0fe01b8726f4fc88f3
devphank <164417807+devphank@users.noreply.github.com>
2025-04-21 15:09:12 -0700

    Update main_page.py

--- main_page.py
+++ main_page.py
@@ -1,48 +1,42 @@
 import streamlit as st
-import pandas as pd 
-import numpy as np
-
 from datetime import datetime
-# import os
-# import gspread
 
-
-# for it to pop up on the sidebar
+# Sidebar
 st.sidebar.markdown("# Home 🏠")
 
-
-# title
+# Title
 st.title('EOC Check-In/Check-Out System')
 
-# introduction
-st.write = ("Hi! Welcome to EOC's Check-In/Check Out System")
-st.write += ("Please come and explore our applications for your needs!")
+# Introduction
+st.write("Hi! Welcome to EOC's Check-In/Check-Out System. Please come and explore our applications for your needs!")
 
-# for table of contents
-st.subheader('For Users', divider= 'blue')
+# For Users Section
+st.subheader('For Users', divider='blue')
 st.text("Come explore our pages! We aim to help make sure you are registered properly.") 
 
 col1, col2 = st.columns(2)
+
 with col1:
-   st.text("To scan your personal QR Codes:")
-   
-   st.text("for the GOLD Team:")
-   st.page_link("pages/Gold_Check_In_Form.py", label="Check In Form", icon="📋")
-   st.page_link("pages/Gold_Check_Out_Form.py", label="Check Out Form", icon="🔒")
+    st.text("To scan your personal QR Codes:")
+    st.text("For the GOLD Team:")
+    st.page_link("pages/Gold_Check_In_Form.py", label="Check In Form", icon="📋")
+    st.page_link("pages/Gold_Check_Out_Form.py", label="Check Out Form", icon="🔒")
+
 with col2:
-   st.page_link("pages/QR_Scanner.py", label="QR Scanner", icon="🔒")
+    st.page_link("pages/QR_Scanner.py", label="QR Scanner", icon="📷")
+    st.text("For the BLUE Team:")
+    st.page_link("pages/Blue_Check_In_Form.py", label="Check In Form", icon="📋")
+    st.page_link("pages/Blue_Check_Out_Form.py", label="Check Out Form", icon="🔒")
 
-   st.text("for the BLUE Team:")
-   st.page_link("pages/Blue_Check_In_Form.py", label="Check In Form", icon="📋")
-   st.page_link("pages/Blue_Check_Out_Form.py", label="Check Out Form", icon="🔒")
+# For Admin Section
+st.subheader('For Administrators', divider='blue')
 
-st.subheader('For Administrators', divider= 'blue')
 col3, col4 = st.columns(2)
 with col3:
-   st.page_link("pages/Admin_DashBoard.py", label="Admin DashBoard", icon="🔒")
+    st.page_link("pages/Admin_DashBoard.py", label="Admin Dashboard", icon="📊")
 with col4:
-   st.page_link("pages/QR_Generator.py", label="QR Generator", icon="🔒")
+    st.page_link("pages/QR_Generator.py", label="QR Generator", icon="🧾")
 
-# footer
+# Footer
 st.markdown("---")
-st.caption("Provided by SJSU • Powered by Streamlit ")
+st.caption("Provided by SJSU • Powered by Streamlit")
